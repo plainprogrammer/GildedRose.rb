b97af8ca668ed7cf66259232a637f4181611ff37
James Thompson <james@thomps.onl>
2021-10-27 11:54:16 -0400

    Wrap item struct in a decorator to give us a place to hang behavior.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,9 @@
+class ItemDecorator < SimpleDelegator; end
+
 def update_quality(items)
   items.each do |item|
+    item = ItemDecorator.new(item)
+
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
         if item.name != 'Sulfuras, Hand of Ragnaros'
