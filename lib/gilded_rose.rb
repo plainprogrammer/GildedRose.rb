728bea8b3ce5e1ed68c4ec7e299e0b8464983d27
James Thompson <james@thomps.onl>
2021-10-28 14:21:13 -0400

    Inlined checks for backstage passes time until sell-in passes.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,12 +17,8 @@ def update(item)
     adjust_quality(item, 1) if item.sell_in < 0
   when 'Backstage passes to a TAFKAL80ETC concert'
     adjust_quality(item, 1)
-    if item.sell_in < 11
-      adjust_quality(item, 1)
-    end
-    if item.sell_in < 6
-      adjust_quality(item, 1)
-    end
+    adjust_quality(item, 1) if item.sell_in < 11
+    adjust_quality(item, 1) if item.sell_in < 6
     decrement_sell_in(item)
     adjust_quality(item, -item.quality) if item.sell_in < 0
   when 'Sulfuras, Hand of Ragnaros'
