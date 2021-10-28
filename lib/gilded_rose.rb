af5209e285211c9db5e7fd6efa34d993d73fdce7
James Thompson <james@thomps.onl>
2021-10-28 11:44:09 -0400

    Moved quality guard clauses into ItemDecorator.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,10 +1,10 @@
 class ItemDecorator < SimpleDelegator
   def decrement_quality
-    self.quality -= 1
+    self.quality -= 1 if self.quality > 0
   end
 
   def increment_quality
-    self.quality += 1
+    self.quality += 1 if self.quality < 50
   end
 
   def decrement_sell_in
@@ -21,26 +21,18 @@ def update_quality(items)
     item = ItemDecorator.new(item)
 
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.decrement_quality
-        end
+      if item.name != 'Sulfuras, Hand of Ragnaros'
+        item.decrement_quality
       end
     else
-      if item.quality < 50
-        item.increment_quality
+      item.increment_quality
 
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.increment_quality
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.increment_quality
-            end
-          end
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in < 11
+          item.increment_quality
+        end
+        if item.sell_in < 6
+          item.increment_quality
         end
       end
     end
@@ -50,18 +42,14 @@ def update_quality(items)
     if item.sell_in < 0
       if item.name != "Aged Brie"
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.decrement_quality
-            end
+          if item.name != 'Sulfuras, Hand of Ragnaros'
+            item.decrement_quality
           end
         else
           item.zero_out_quality
         end
       else
-        if item.quality < 50
-          item.increment_quality
-        end
+        item.increment_quality
       end
     end
   end
