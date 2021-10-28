8c223dd705effb93a55cd8c896f402e27de944bc
James Thompson <james@thomps.onl>
2021-10-28 14:39:56 -0400

    Refactor quality guard cases in adjust_quality method.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,8 +1,7 @@
 def adjust_quality(item, amount)
-  return if amount > 0 && item.quality == 50
-  return if amount < 0 && item.quality == 0
-
   item.quality += amount
+  item.quality = 50 if item.quality > 50
+  item.quality = 0 if item.quality < 0
 end
 
 def decrement_sell_in(item)
