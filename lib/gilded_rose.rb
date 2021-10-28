f71f689f4751bbefbb050c480d8ac7ded7af1318
James Thompson <james@thomps.onl>
2021-10-28 14:20:06 -0400

    Extract case statement into update method.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,31 +9,33 @@ def decrement_sell_in(item)
   item.sell_in -= 1
 end
 
-def update_quality(items)
-  items.each do |item|
-    case item.name
-    when 'Aged Brie'
+def update(item)
+  case item.name
+  when 'Aged Brie'
+    adjust_quality(item, 1)
+    decrement_sell_in(item)
+    adjust_quality(item, 1) if item.sell_in < 0
+  when 'Backstage passes to a TAFKAL80ETC concert'
+    adjust_quality(item, 1)
+    if item.sell_in < 11
       adjust_quality(item, 1)
-      decrement_sell_in(item)
-      adjust_quality(item, 1) if item.sell_in < 0
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      adjust_quality(item, 1)
-      if item.sell_in < 11
-        adjust_quality(item, 1)
-      end
-      if item.sell_in < 6
-        adjust_quality(item, 1)
-      end
-      decrement_sell_in(item)
-      adjust_quality(item, -item.quality) if item.sell_in < 0
-    when 'Sulfuras, Hand of Ragnaros'
-      # No-Op
-    else
-      adjust_quality(item, -1)
-      decrement_sell_in(item)
-      adjust_quality(item, -1) if item.sell_in < 0
     end
+    if item.sell_in < 6
+      adjust_quality(item, 1)
+    end
+    decrement_sell_in(item)
+    adjust_quality(item, -item.quality) if item.sell_in < 0
+  when 'Sulfuras, Hand of Ragnaros'
+    # No-Op
+  else
+    adjust_quality(item, -1)
+    decrement_sell_in(item)
+    adjust_quality(item, -1) if item.sell_in < 0
   end
+end
+
+def update_quality(items)
+  items.each(&method(:update))
 end
 
 #----------------------------
