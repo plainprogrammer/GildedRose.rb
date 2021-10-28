592a88e86617eb26779ce9066cca89753dd0acc4
James Thompson <james@thomps.onl>
2021-10-28 14:40:46 -0400

    Refactor internals of update method to group all quality adjustments together.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,21 +11,35 @@ end
 def update(item)
   case item.name
   when 'Aged Brie'
-    adjust_quality(item, 1)
     decrement_sell_in(item)
-    adjust_quality(item, 1) if item.sell_in < 0
+
+    if item.sell_in < 0
+      adjust_quality(item, 2)
+    else
+      adjust_quality(item, 1)
+    end
   when 'Backstage passes to a TAFKAL80ETC concert'
-    adjust_quality(item, 1)
-    adjust_quality(item, 1) if item.sell_in < 11
-    adjust_quality(item, 1) if item.sell_in < 6
     decrement_sell_in(item)
-    adjust_quality(item, -item.quality) if item.sell_in < 0
+
+    if item.sell_in < 0
+      adjust_quality(item, -item.quality)
+    elsif item.sell_in >= 10
+      adjust_quality(item, 1)
+    elsif item.sell_in >= 5
+      adjust_quality(item, 2)
+    else
+      adjust_quality(item, 3)
+    end
   when 'Sulfuras, Hand of Ragnaros'
     # No-Op
-  else
-    adjust_quality(item, -1)
+  else # Normal Item
     decrement_sell_in(item)
-    adjust_quality(item, -1) if item.sell_in < 0
+
+    if item.sell_in < 0
+      adjust_quality(item, -2)
+    else
+      adjust_quality(item, -1)
+    end
   end
 end
 
