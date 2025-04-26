65ebf21d59035bcbaf3e0e4a5dee11f2e0788190
James Thompson <james@thomps.onl>
2025-04-26 19:55:08 -0400

    GPT-4o Solution

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,48 +1,34 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
+    # Skip Sulfuras updates
+    next if item.name == 'Sulfuras, Hand of Ragnaros'
+
+    # Update sell_in for non-legendary items
+    item.sell_in -= 1
+
+    # Handle quality updates based on item type
+    case item.name
+    when 'Aged Brie'
+      increase = (item.sell_in < 0) ? 2 : 1
+      item.quality += increase
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      if item.sell_in < 0
+        item.quality = 0
+      elsif item.sell_in < 5
+        item.quality += 3
+      elsif item.sell_in < 10
+        item.quality += 2
       else
-        if item.quality < 50
-          item.quality += 1
-        end
+        item.quality += 1
       end
+    else # Normal and Conjured items
+      decrease = (item.sell_in < 0) ? 2 : 1
+      decrease *= 2 if item.name.start_with?('Conjured') # Conjured items degrade twice as fast
+      item.quality -= decrease
     end
+
+    # Apply quality constraints (0 <= quality <= 50)
+    item.quality = [[item.quality, 0].max, 50].min
   end
 end
 
@@ -51,3 +37,4 @@ end
 #----------------------------
 
 Item = Struct.new(:name, :sell_in, :quality)
+
