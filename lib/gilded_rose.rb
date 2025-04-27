aa238376c577b57a7cf978553570b3693da59d32
James Thompson <james@thomps.onl>
2025-04-26 20:27:36 -0400

    Gemini 2.5 Pro's solution.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,50 +1,101 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+    updater = ItemUpdater.for(item)
+    updater.update
+  end
+end
+
+# Helper class to encapsulate update logic for different item types
+class ItemUpdater
+  MAX_QUALITY = 50
+  MIN_QUALITY = 0
+
+  attr_reader :item
+
+  def self.for(item)
+    case item.name
+    when 'Aged Brie'
+      AgedBrieUpdater.new(item)
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      BackstagePassUpdater.new(item)
+    when 'Sulfuras, Hand of Ragnaros'
+      SulfurasUpdater.new(item)
+    when /^Conjured/ # Match names starting with "Conjured"
+      ConjuredItemUpdater.new(item)
     else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
+      NormalItemUpdater.new(item)
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+  end
+
+  def initialize(item)
+    @item = item
+  end
+
+  # Default update behavior (for normal items)
+  def update
+    decrease_sell_in
+    decrease_quality
+    decrease_quality if expired?
+  end
+
+  protected
+
+  def decrease_sell_in
+    item.sell_in -= 1
+  end
+
+  def expired?
+    item.sell_in < 0
+  end
+
+  def increase_quality(amount = 1)
+    item.quality = [item.quality + amount, MAX_QUALITY].min
+  end
+
+  def decrease_quality(amount = 1)
+    item.quality = [item.quality - amount, MIN_QUALITY].max
+  end
+end
+
+class AgedBrieUpdater < ItemUpdater
+  def update
+    decrease_sell_in
+    increase_quality
+    increase_quality if expired?
+  end
+end
+
+class BackstagePassUpdater < ItemUpdater
+  def update
+    decrease_sell_in
+    if expired?
+      item.quality = 0
+    else
+      increase_quality
+      increase_quality if item.sell_in < 10
+      increase_quality if item.sell_in < 5
     end
   end
 end
+
+class SulfurasUpdater < ItemUpdater
+  # Sulfuras never changes
+  def update; end
+end
+
+class NormalItemUpdater < ItemUpdater
+  # Uses default update logic
+end
+
+class ConjuredItemUpdater < ItemUpdater
+  # Conjured items degrade twice as fast
+  def update
+    decrease_sell_in
+    decrease_quality(2)
+    decrease_quality(2) if expired?
+  end
+end
+
 
 #----------------------------
 # DO NOT CHANGE THINGS BELOW
