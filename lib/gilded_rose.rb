7966fbb34f17acc21b3aa4e9b9830cb703f0529c
James Thompson <james.thompson@fcpeuro.com>
2025-05-06 17:45:12 -0400

    Solution

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,49 +1,85 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+class Sulfuras
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    # Sulfuras does not change in quality or sell_in
+  end
+end
+
+class BackstagePass
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    if @item.quality < 50
+      @item.quality += 1
+      @item.quality += 1 if @item.sell_in < 11 && @item.quality < 50
+      @item.quality += 1 if @item.sell_in < 6 && @item.quality < 50
+    end
+    @item.sell_in -= 1
+    @item.quality = 0 if @item.sell_in < 0
+  end
+end
+
+class AgedBrie
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    @item.quality += 1 if @item.quality < 50
+    @item.sell_in -= 1
+    @item.quality += 1 if @item.sell_in < 0 && @item.quality < 50
+  end
+end
+
+class NormalItem
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    @item.quality -= 1 if @item.quality > 0
+    @item.sell_in -= 1
+    @item.quality -= 1 if @item.sell_in < 0 && @item.quality > 0
+  end
+end
+
+class ConjuredItem
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    @item.quality -= 2 if @item.quality > 0
+    @item.sell_in -= 1
+    @item.quality -= 2 if @item.sell_in < 0 && @item.quality > 0
+    @item.quality = 0 if @item.quality < 0
+  end
+end
+
+class ItemFactory
+  def self.create(item)
+    case item.name
+    when 'Sulfuras, Hand of Ragnaros'
+      Sulfuras.new(item)
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      BackstagePass.new(item)
+    when 'Aged Brie'
+      AgedBrie.new(item)
+    when /^Conjured/
+      ConjuredItem.new(item)
     else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+      NormalItem.new(item)
     end
   end
+end
+
+def update_quality(items)
+  items.each { |item| ItemFactory.create(item).update }
 end
 
 #----------------------------
@@ -51,3 +87,4 @@ end
 #----------------------------
 
 Item = Struct.new(:name, :sell_in, :quality)
+
